1c3c7b7de34d9ba3228b3b255d80f137c6694047
Dustin Baldwin <ipalindromi@gmail.com>
2019-06-02 19:15:17 -0400

    Adds a simple view component

--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
 import React from 'react';
 import reactDom from 'react-dom';
 
-reactDom.render(<div>Welcome to the scaffold!</div>, document.querySelector('main'));
+import View from './components/dumb/View';
+
+reactDom.render(
+	<View>Welcome to the scaffold! (We have fun and games)</View>,
+	document.querySelector('main'),
+);
