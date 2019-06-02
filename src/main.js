349e7c35f5fa3fc38df288118f2465b7bec1ef62
Dustin Baldwin <ipalindromi@gmail.com>
2019-06-02 19:58:58 -0400

    Add basic redux with react-redux

--- src/main.js
+++ src/main.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import reactDom from 'react-dom';
+import { Provider } from 'react-redux';
+
+import store from './redux/store';
 
 import View from './components/dumb/View';
 
 reactDom.render(
-	<View>Welcome to the scaffold! (We have fun and games)</View>,
+	<Provider store={store}>
+		<View>Welcome to the scaffold! (We have fun and games)</View>
+	</Provider>,
 	document.querySelector('main'),
 );
